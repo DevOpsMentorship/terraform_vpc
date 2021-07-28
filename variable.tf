6a0f8de662b2b8a7995fd79d55c9e821d097c5ed
vladyslav.tkachuk <vladyslav.tkachuk99@gmail.com>
2021-07-28 19:54:33 +0300

    added public subnet

--- variable.tf
+++ variable.tf
@@ -1,9 +1,15 @@
 variable "aws_region" {
-  default    = "eu-west-2"
+  default     = "eu-west-2"
 }
 
 variable "vpc_cidr" {
   default     = "10.0.0.0/16"
   description = "vpc_cidr_block"
+  type        = string
+}
+
+variable "subnet_cidr" {
+  default     = "10.0.1.0/24"
+  description = "subnet_cidr_block"
   type        = string
 }
--- vpc.tf
+++ vpc.tf
@@ -17,3 +17,15 @@ resource "aws_internet_gateway" "gateway" {
     Name    = "gateway"
   }
 }
+
+# Create Public Subnet
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.subnet_cidr
+  availability_zone       = "eu-west-2"
+  map_public_ip_on_launch = true
+
+  tags      = {
+    Name    = "public_subnet"
+  }
+}
