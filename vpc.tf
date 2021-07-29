d94d66ea7af85f9ad6538c508e233c0962ecb888
vladyslav.tkachuk <vladyslav.tkachuk99@gmail.com>
2021-07-29 13:31:40 +0300

    added new block

--- vpc.tf
+++ vpc.tf
@@ -44,3 +44,8 @@ resource "aws_route_table" "public_route_table" {
   }
 }
 
+# Associate Public Subnet to "Public Route Table"
+resource "aws_route_table_association" "public_subnet_route_table_association" {
+  subnet_id           = aws_subnet.public_subnet.id
+  route_table_id      = aws_route_table.public_route_table.id
+}
