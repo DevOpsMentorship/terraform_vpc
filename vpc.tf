60b1d56c840a9fa5028815082305a32c05eb3db0
vladyslav.tkachuk <vladyslav.tkachuk99@gmail.com>
2021-07-29 12:00:22 +0300

    change description

--- vpc.tf
+++ vpc.tf
@@ -30,7 +30,7 @@ resource "aws_subnet" "public_subnet" {
   }
 }
 
-# Create Route Table and Add Public Route
+# Create Route Table and add public route
 resource "aws_route_table" "public_route_table" {
   vpc_id       = aws_vpc.vpc.id
 
