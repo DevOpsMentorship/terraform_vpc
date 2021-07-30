08d77e1ca6b1b025c7b3338b7d74b0a3ea5e92d1
vladyslav.tkachuk <vladyslav.tkachuk99@gmail.com>
2021-07-30 07:38:39 +0300

    change syntax

--- variable.tf
+++ variable.tf
@@ -21,7 +21,7 @@ variable "route_table_cidr" {
 }
 
 variable "private_subnet_cidr" {
-  default = "10.0.1.0/24"
+  default     = "10.0.1.0/24"
   description = "private_subnet"
-  type = string
+  type        = string
 }
