d7e70771b16560412a2727369707b3d5d2017e55
vladyslav.tkachuk <vladyslav.tkachuk99@gmail.com>
2021-07-31 13:16:27 +0300

    change syntax

--- vpc.tf
+++ vpc.tf
@@ -16,19 +16,17 @@ resource "aws_internet_gateway" "gateway" {
     Name    = "gateway"
   }
 }
-
 # Create Public Subnet
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = var.public_subnet_cidr
-  availability_zone       = "eu-west-2b"
+  availability_zone       = "eu-west-2a"
   map_public_ip_on_launch = true
 
   tags      = {
     Name    = "public_subnet"
   }
 }
-
 # Create Route Table and add Public Route
 resource "aws_route_table" "public_route_table" {
   vpc_id       = aws_vpc.vpc.id
@@ -42,13 +40,11 @@ resource "aws_route_table" "public_route_table" {
     Name     = "route_table"
   }
 }
-
 # Associate Public Subnet to "Public Route Table"
 resource "aws_route_table_association" "public_subnet_route_table_association" {
   subnet_id           = aws_subnet.public_subnet.id
   route_table_id      = aws_route_table.public_route_table.id
 }
-
 # Create Private Subnet
 resource "aws_subnet" "private_subnet" {
   vpc_id                   = aws_vpc.vpc.id
